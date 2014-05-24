08789333e4a86e264c17d9faafd88f93e108cead
MineMaarten <loopycoaster_8@hotmail.com>
2014-05-24 14:48:20 +0200

    Made the items and blocks initialize properly

--- src/main/java/net/quetzi/bluepower/init/Blocks.java
+++ src/main/java/net/quetzi/bluepower/init/Blocks.java
@@ -10,17 +10,17 @@ import net.quetzi.bluepower.blocks.ContainerAlloyFurnace;
 import net.quetzi.bluepower.references.Refs;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-public class Blocks {
-    public static void init() {
+public class Blocks{
+    public static void init(){
         registerBlocks();
     }
-    
-    public static final Block basalt = (Block)Block.blockRegistry.getObject(Refs.BASALT_NAME);
-    public static final Block marble = (Block)Block.blockRegistry.getObject(Refs.MARBLE_NAME);
-    public static final Block basalt_cobble = (Block)Block.blockRegistry.getObject(Refs.BASALTCOBBLE_NAME);
-    public static final Block basalt_brick = (Block)Block.blockRegistry.getObject(Refs.BASALTBRICK_NAME);
-    public static final Block marble_brick = (Block)Block.blockRegistry.getObject(Refs.MARBLEBRICK_NAME);
-    
+
+    public static final Block basalt = new BlockStoneOre(Refs.BASALT_NAME);
+    public static final Block marble = new BlockStoneOre(Refs.MARBLE_NAME);
+    public static final Block basalt_cobble = new BlockStoneOre(Refs.BASALTCOBBLE_NAME);
+    public static final Block basalt_brick = new BlockStoneOre(Refs.BASALTBRICK_NAME);
+    public static final Block marble_brick = new BlockStoneOre(Refs.MARBLEBRICK_NAME);
+
     public static final Block nikolite_ore = new BlockNikoliteOre(Refs.NIKOLITEORE_NAME);
     public static final Block ruby_ore = new BlockGemOre(Refs.RUBYORE_NAME);
     public static final Block sapphire_ore = new BlockGemOre(Refs.SAPPHIREORE_NAME);
@@ -28,31 +28,31 @@ public class Blocks {
     public static final Block copper_ore = new BlockStoneOre(Refs.COPPERORE_NAME);
     public static final Block silver_ore = new BlockStoneOre(Refs.SILVERORE_NAME);
     public static final Block tin_ore = new BlockStoneOre(Refs.TINORE_NAME);
-    
-    public static final Block ruby_block = (Block)Block.blockRegistry.getObject(Refs.RUBYBLOCK_NAME);
-    public static final Block sapphire_block = (Block)Block.blockRegistry.getObject(Refs.SAPPHIREBLOCK_NAME);
-    public static final Block malachite_block = (Block)Block.blockRegistry.getObject(Refs.MALACHITEBLOCK_NAME);
-    public static final Block nikolite_block = (Block)Block.blockRegistry.getObject(Refs.NIKOLITEBLOCK_NAME);
-    
-    private static void registerBlocks() {
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.BASALT_NAME), Refs.BASALT_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.MARBLE_NAME), Refs.MARBLE_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.BASALTCOBBLE_NAME), Refs.BASALTCOBBLE_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.BASALTBRICK_NAME), Refs.BASALTBRICK_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.MARBLEBRICK_NAME), Refs.MARBLEBRICK_NAME);
+
+    public static final Block ruby_block = new BlockStoneOre(Refs.RUBYBLOCK_NAME);
+    public static final Block sapphire_block = new BlockStoneOre(Refs.SAPPHIREBLOCK_NAME);
+    public static final Block malachite_block = new BlockStoneOre(Refs.MALACHITEBLOCK_NAME);
+    public static final Block nikolite_block = new BlockNikoliteBlock(Refs.NIKOLITEBLOCK_NAME);
+
+    private static void registerBlocks(){
+        GameRegistry.registerBlock(basalt, Refs.BASALT_NAME);
+        GameRegistry.registerBlock(marble, Refs.MARBLE_NAME);
+        GameRegistry.registerBlock(basalt_cobble, Refs.BASALTCOBBLE_NAME);
+        GameRegistry.registerBlock(basalt_brick, Refs.BASALTBRICK_NAME);
+        GameRegistry.registerBlock(marble_brick, Refs.MARBLEBRICK_NAME);
         GameRegistry.registerBlock(new ContainerAlloyFurnace().setBlockName(Refs.ALLOYFURNACE_NAME), Refs.ALLOYFURNACE_NAME);
-        GameRegistry.registerBlock(new BlockNikoliteOre(Refs.NIKOLITEORE_NAME), Refs.NIKOLITEORE_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.COPPERORE_NAME), Refs.COPPERORE_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.SILVERORE_NAME), Refs.SILVERORE_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.TINORE_NAME), Refs.TINORE_NAME);
-        GameRegistry.registerBlock(new BlockGemOre(Refs.RUBYORE_NAME), Refs.RUBYORE_NAME);
-        GameRegistry.registerBlock(new BlockGemOre(Refs.SAPPHIREORE_NAME), Refs.SAPPHIREORE_NAME);
-        GameRegistry.registerBlock(new BlockGemOre(Refs.MALACHITEORE_NAME), Refs.MALACHITEORE_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.RUBYBLOCK_NAME), Refs.RUBYBLOCK_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.SAPPHIREBLOCK_NAME), Refs.SAPPHIREBLOCK_NAME);
-        GameRegistry.registerBlock(new BlockStoneOre(Refs.MALACHITEBLOCK_NAME), Refs.MALACHITEBLOCK_NAME);
-        GameRegistry.registerBlock(new BlockNikoliteBlock(Refs.NIKOLITEBLOCK_NAME), Refs.NIKOLITEBLOCK_NAME);
-        
+        GameRegistry.registerBlock(nikolite_ore, Refs.NIKOLITEORE_NAME);
+        GameRegistry.registerBlock(copper_ore, Refs.COPPERORE_NAME);
+        GameRegistry.registerBlock(silver_ore, Refs.SILVERORE_NAME);
+        GameRegistry.registerBlock(tin_ore, Refs.TINORE_NAME);
+        GameRegistry.registerBlock(ruby_ore, Refs.RUBYORE_NAME);
+        GameRegistry.registerBlock(sapphire_ore, Refs.SAPPHIREORE_NAME);
+        GameRegistry.registerBlock(malachite_ore, Refs.MALACHITEORE_NAME);
+        GameRegistry.registerBlock(ruby_block, Refs.RUBYBLOCK_NAME);
+        GameRegistry.registerBlock(sapphire_block, Refs.SAPPHIREBLOCK_NAME);
+        GameRegistry.registerBlock(malachite_block, Refs.MALACHITEBLOCK_NAME);
+        GameRegistry.registerBlock(nikolite_block, Refs.NIKOLITEBLOCK_NAME);
+
         OreDictionary.registerOre("oreCopper", Blocks.copper_ore);
         OreDictionary.registerOre("oreTin", Blocks.tin_ore);
         OreDictionary.registerOre("oreSilver", Blocks.silver_ore);
--- src/main/java/net/quetzi/bluepower/init/Items.java
+++ src/main/java/net/quetzi/bluepower/init/Items.java
@@ -8,17 +8,16 @@ import net.quetzi.bluepower.items.ItemSapphire;
 import net.quetzi.bluepower.references.Refs;
 import cpw.mods.fml.common.registry.GameRegistry;
 
+public class Items{
+    public static final Item malachite = new ItemMalachite();
+    public static final Item sapphire = new ItemSapphire();
+    public static final Item ruby = new ItemRuby();
+    public static final Item nikolite = new ItemNikolite();
 
-public class Items {
-    public static void init() {
-        GameRegistry.registerItem(new ItemMalachite(), Refs.ITEMMALACHITE_NAME);
-        GameRegistry.registerItem(new ItemSapphire(), Refs.ITEMSAPPHIRE_NAME);
-        GameRegistry.registerItem(new ItemRuby(), Refs.ITEMRUBY_NAME);
-        GameRegistry.registerItem(new ItemNikolite(), Refs.ITEMNIKOLITE_NAME);
+    public static void init(){
+        GameRegistry.registerItem(malachite, Refs.ITEMMALACHITE_NAME);
+        GameRegistry.registerItem(sapphire, Refs.ITEMSAPPHIRE_NAME);
+        GameRegistry.registerItem(ruby, Refs.ITEMRUBY_NAME);
+        GameRegistry.registerItem(nikolite, Refs.ITEMNIKOLITE_NAME);
     }
-    
-    public static final Item malachite = (Item)Item.itemRegistry.getObject(Refs.ITEMMALACHITE_NAME);
-    public static final Item sapphire = (Item)Item.itemRegistry.getObject(Refs.ITEMSAPPHIRE_NAME);
-    public static final Item ruby = (Item)Item.itemRegistry.getObject(Refs.ITEMRUBY_NAME);
-    public static final Item nikolite = (Item)Item.itemRegistry.getObject(Refs.ITEMNIKOLITE_NAME);
 }
