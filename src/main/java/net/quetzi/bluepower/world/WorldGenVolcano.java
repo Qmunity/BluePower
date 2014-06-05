7dc440ef1df67cf0a3bade93d75f6b74e4604a89
MineMaarten <loopycoaster_8@hotmail.com>
2014-06-05 19:37:15 +0200

    Rewrote volcano generation code (still WIP)

--- src/main/java/net/quetzi/bluepower/world/WorldGenVolcano.java
+++ src/main/java/net/quetzi/bluepower/world/WorldGenVolcano.java
@@ -1,141 +1,105 @@
 package net.quetzi.bluepower.world;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockFlower;
-import net.minecraft.init.Blocks;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.quetzi.bluepower.init.BPBlocks;
 
-import java.util.*;
+public class WorldGenVolcano{
+    public class Pos{
+        public final int x, z;
 
-public class WorldGenVolcano extends WorldGenMinable {
-    LinkedList layerFill = new LinkedList();
-    HashMap currentLayerFill = new HashMap();
-    private Block block;
-    private int numberOfBlocks;
-
-    public WorldGenVolcano(Block block, int num) {
-        super(block, num);
-        this.block = block;
-        this.numberOfBlocks = num;
-    }
-
-    private void addBlock(int x, int y, int z, int sides) {
-        if (sides <= 0)
-            return;
-        List searchBlock = Arrays.asList(x, z);
-        Integer i = (Integer) this.currentLayerFill.get(searchBlock);
-
-        if ((i != null) && (sides <= i))
-            return;
-        this.layerFill.addLast(Arrays.asList(x, y, z));
-
-        this.currentLayerFill.put(searchBlock, sides);
-    }
-
-    private void searchBlock(int x, int y, int z, int sides, Random random) {
-        int rp = random.nextInt(16);
-        addBlock(x - 1, y, z, (rp & 0x1) > 0 ? sides - 1 : sides);
-        addBlock(x + 1, y, z, (rp & 0x2) > 0 ? sides - 1 : sides);
-        addBlock(x, y, z - 1, (rp & 0x4) > 0 ? sides - 1 : sides);
-        addBlock(x, y, z + 1, (rp & 0x8) > 0 ? sides - 1 : sides);
-    }
-
-    public boolean canReplaceBlock(Block block) {
-        if ((block == Blocks.air) || (block == Blocks.water) || (block == Blocks.flowing_water) || (block instanceof BlockFlower) || (block == Blocks.grass) || (block == Blocks.dirt) || (block == Blocks.log) || (block == Blocks.log2) || (block == Blocks.leaves) || (block == Blocks.leaves2)) {
-            return true;
-        }
-        return false;
-    }
-
-    public void clearArea(World world, int x, int y, int z) {
-        Block block = world.getBlock(x, y, z);
-        if (block == Blocks.air) {
-            return;
-        }
-        if ((block != Blocks.log) && (block != Blocks.log2) && (block != Blocks.leaves)
-                && (block != Blocks.leaves2) && (block != Blocks.snow_layer) && (block != Blocks.vine)) {
-            return;
-        }
-        world.setBlock(x, y, z, Blocks.air);
-        clearArea(world, x, y + 1, z);
-    }
-
-    private void genLavaColumn(World world, int x, int y, int z) {
-        int height = world.getHeightValue(x, z);
-        // Create column of lava with basalt on 4 sides down to bedrock
-        while (canReplaceBlock(world.getBlock(x, height - 1, z)))
-            height--;
-        for (int i = y; i < height; i++) {
-            world.setBlock(x, i, z, Blocks.lava);
-            world.setBlock(x - 1, i, z, this.block);
-            world.setBlock(x + 1, i, z, this.block);
-            world.setBlock(x, i, z - 1, this.block);
-            world.setBlock(x, i, z + 1, this.block);
+        public Pos(int x, int z){
+            this.x = x;
+            this.z = z;
         }
     }
 
-    public boolean generate(World world, Random random, int x, int y, int z) {
-        if (world.getBlock(x, y, z) != Blocks.lava) {
-            return false;
-        }
-        genLavaColumn(world, x, y, z);
-        int head = 3 + random.nextInt(4);
-        int spread = random.nextInt(3);
-        int surfaceHeight = world.getHeightValue(x, z);
-        int currentY = surfaceHeight;
+    private final Map<Pos, Integer> vulcanoMap = new HashMap<Pos, Integer>();
+    private static List<Pos>[] distMap;//Saves an array of relative Positions with distance to origin. The index is the distance, the element the positions with that distance to the origin.
+    private static final int MAX_VULCANO_RADIUS = 50;//absulute max radius a vulcano can have, this should be a magnitude bigger than an average vulcano radius.
 
-        // While there are blocks left to add
-        while (this.numberOfBlocks > 0) {
-            boolean atTop = false;
-            // if the layer is empty we need to fill it
-            while (this.layerFill.size() == 0) {
-                world.setBlock(x, currentY, z, Blocks.lava); // set the centre block to lava
-                this.currentLayerFill.clear(); //clear out the temporary layer data
-                searchBlock(x, currentY, z, head, random);
-                currentY++;
-                if (currentY > 200) {
-                    world.setBlock(x, currentY, z, Blocks.flowing_lava);
-                    while ((currentY > surfaceHeight) && (world.getBlock(x, currentY, z) == Blocks.lava)) {
-                        world.markBlockForUpdate(x, currentY, z);
-                        world.notifyBlocksOfNeighborChange(x, currentY, z, Blocks.lava);
-                        world.scheduledUpdatesAreImmediate = true;
-                        Blocks.lava.updateTick(world, x, currentY, z, random);
-
-                        world.scheduledUpdatesAreImmediate = false;
-                        currentY--;
+    public void generate(World world, Random rand, int middleX, int vulcanoHeight, int middleZ){
+        if(rand.nextInt(200) != 0) return;
+        if(distMap == null) calculateDistMap();
+        long start = System.currentTimeMillis();
+        boolean first = true;
+        for(List<Pos> distList : distMap) {//Loop through every XZ position of the vulcano, in order of how close the positions are from the center. The vulcano will be generated from the center to the edge.
+            boolean isFinished = true;//Will stay true as long as there were still blocks being generated at this distance from the vulcano.
+            for(Pos p : distList) {
+                int posHeight = first ? vulcanoHeight : getNewVulcanoHeight(p, rand);
+                first = false;
+                int worldHeight = world.getHeightValue(p.x + middleX, p.z + middleZ) - 1;
+                if(posHeight > worldHeight) {// If the calculated desired vulcano height is higher than the world height, generate.
+                    vulcanoMap.put(p, posHeight);
+                    for(int i = worldHeight + 1; i <= posHeight; i++) {
+                        world.setBlock(p.x + middleX, i, p.z + middleZ, BPBlocks.basalt, 0, 0);
                     }
-                    atTop = true;
-                    break;
+                    isFinished = false;
                 }
             }
-            if (atTop) {
-                break;
-            }
+            if(isFinished) break;
+        }
+        System.out.println("time: " + (System.currentTimeMillis() - start));
+    }
 
-            Integer[] blockCoord = (Integer[]) ((List) this.layerFill.removeFirst()).toArray();
-
-            if (world.blockExists(blockCoord[0], 64, blockCoord[2])) {
-                int pow = (Integer) this.currentLayerFill.get(Arrays.asList(blockCoord[0], blockCoord[2]));
-                int currentYIndex = world.getHeightValue(blockCoord[0], blockCoord[2]);
-
-                while ((currentYIndex <= blockCoord[1]) && (canReplaceBlock(world.getBlock(blockCoord[0], currentYIndex - 1, blockCoord[2]))))
-                    currentYIndex--;
-                if (currentYIndex <= blockCoord[1]) {
-                    Block block = world.getBlock(blockCoord[0], currentYIndex, blockCoord[2]);
-                    if (canReplaceBlock(block)) {
-                        clearArea(world, blockCoord[0], currentYIndex, blockCoord[2]);
-                        world.setBlock(blockCoord[0], currentYIndex, blockCoord[2], this.block);
-
-                        if (blockCoord[1] > currentYIndex) {
-                            pow = Math.max(pow, spread);
-                        }
-                        searchBlock(blockCoord[0], currentYIndex, blockCoord[2], pow, random);
-                        this.numberOfBlocks -= 1;
+    @SuppressWarnings("unchecked")
+    private void calculateDistMap(){
+        long start = System.currentTimeMillis();
+        distMap = new List[MAX_VULCANO_RADIUS];
+        for(int x = -MAX_VULCANO_RADIUS; x <= MAX_VULCANO_RADIUS; x++) {
+            for(int z = -MAX_VULCANO_RADIUS; z <= MAX_VULCANO_RADIUS; z++) {
+                int dist = (int)Math.sqrt(x * x + z * z);
+                if(dist < MAX_VULCANO_RADIUS) {
+                    List<Pos> distList = distMap[dist];
+                    if(distList == null) {
+                        distList = new ArrayList<Pos>();
+                        distMap[dist] = distList;
                     }
+                    distList.add(new Pos(x, z));
                 }
             }
         }
-        return true;
+        System.out.println("time: " + (System.currentTimeMillis() - start));
+    }
+
+    /**
+     * Calculates a height for the requested position. It looks at the adjacent (already generated) vulcano heights, takes the average,
+     * and blends in a bit of randomness. If there are no neighbors this is the first vulcano block generated, meaning it's the center,
+     * meaning it should get the max height.
+     * @param requestedPos
+     * @param maxHeight
+     * @return
+     */
+    private int getNewVulcanoHeight(Pos requestedPos, Random rand){
+        int neighborCount = 0;
+        int totalHeight = 0;
+        for(int x = requestedPos.x - 1; x <= requestedPos.x + 1; x++) {
+            for(int z = requestedPos.z - 1; z <= requestedPos.z + 1; z++) {
+                int neighborHeight = getNeighborHeight(x, z);
+                if(neighborHeight != -1) {
+                    neighborCount++;
+                    totalHeight += neighborHeight;
+                }
+            }
+        }
+        return neighborCount != 0 ? totalHeight / neighborCount - rand.nextInt(2) : -1;
+    }
+
+    /**
+     * This helper method is created so we don't have to create an object just to do a vulcanoMap.get(new Pos(x,z)).
+     * @param x
+     * @param z
+     * @return
+     */
+    private int getNeighborHeight(int x, int z){
+        for(Map.Entry<Pos, Integer> entry : vulcanoMap.entrySet()) {
+            if(entry.getKey().x == x && entry.getKey().z == z) return entry.getValue();
+        }
+        return -1;
     }
 }
--- src/main/java/net/quetzi/bluepower/world/WorldGenerationHandler.java
+++ src/main/java/net/quetzi/bluepower/world/WorldGenerationHandler.java
@@ -67,9 +67,9 @@ public class WorldGenerationHandler implements IWorldGenerator {
         vc *= vc;
         for (int i = 0; i < vc; i++) {
             int x = chunkX * 16 + random.nextInt(16);
-            int y = random.nextInt(22)+10;
+            int y = random.nextInt(40)+80;
             int z = chunkZ * 16 + random.nextInt(16);
-            new WorldGenVolcano(BPBlocks.basalt, random.nextInt(65536)).generate(world, random, x, y, z);
+            new WorldGenVolcano().generate(world, random, x, y, z);
         }
     }
 
