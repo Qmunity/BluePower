58af7e175a7ed921d9026dba9df193fd2108f45e
quetzi <q@quetzi.net>
2014-06-09 10:57:25 +0100

    Fix ore generation issues

--- src/main/java/net/quetzi/bluepower/world/WorldGenerationHandler.java
+++ src/main/java/net/quetzi/bluepower/world/WorldGenerationHandler.java
@@ -37,30 +37,30 @@ public class WorldGenerationHandler implements IWorldGenerator {
     
         if (!world.provider.isSurfaceWorld()) { return; }
         if (Config.generateAmethyst) {
-            this.addOreToGenerate(Config.veinCountAmethyst, Config.veinSizeAmethyst, Config.minAmethystY, Config.maxAmethystY, BPBlocks.amethyst_ore,
+            this.addOreToGenerate(random, Config.veinCountAmethyst, Config.veinSizeAmethyst, Config.minAmethystY, Config.maxAmethystY, BPBlocks.amethyst_ore,
                     world, chunkX, chunkZ);
         }
         if (Config.generateRuby) {
-            this.addOreToGenerate(Config.veinCountRuby, Config.veinSizeRuby, Config.minRubyY, Config.maxRubyY, BPBlocks.ruby_ore, world, chunkX,
+            this.addOreToGenerate(random, Config.veinCountRuby, Config.veinSizeRuby, Config.minRubyY, Config.maxRubyY, BPBlocks.ruby_ore, world, chunkX,
                     chunkZ);
         }
         if (Config.generateSapphire) {
-            this.addOreToGenerate(Config.veinCountSapphire, Config.veinSizeSapphire, Config.minSapphireY, Config.maxSapphireY, BPBlocks.sapphire_ore,
+            this.addOreToGenerate(random, Config.veinCountSapphire, Config.veinSizeSapphire, Config.minSapphireY, Config.maxSapphireY, BPBlocks.sapphire_ore,
                     world, chunkX, chunkZ);
         }
         if (Config.generateSilver) {
-            this.addOreToGenerate(Config.veinCountSilver, Config.veinSizeSilver, Config.minSilverY, Config.maxSilverY, BPBlocks.silver_ore, world,
+            this.addOreToGenerate(random, Config.veinCountSilver, Config.veinSizeSilver, Config.minSilverY, Config.maxSilverY, BPBlocks.silver_ore, world,
                     chunkX, chunkZ);
         }
         if (Config.generateNikolite) {
-            this.addOreToGenerate(Config.veinCountNikolite, Config.veinSizeNikolite, Config.minNikoliteY, Config.maxNikoliteY, BPBlocks.nikolite_ore,
+            this.addOreToGenerate(random, Config.veinCountNikolite, Config.veinSizeNikolite, Config.minNikoliteY, Config.maxNikoliteY, BPBlocks.nikolite_ore,
                     world, chunkX, chunkZ);
         }
         if (Config.generateTin) {
-            this.addOreToGenerate(Config.veinCountTin, Config.veinSizeTin, Config.minTinY, Config.maxTinY, BPBlocks.tin_ore, world, chunkX, chunkZ);
+            this.addOreToGenerate(random, Config.veinCountTin, Config.veinSizeTin, Config.minTinY, Config.maxTinY, BPBlocks.tin_ore, world, chunkX, chunkZ);
         }
         if (Config.generateCopper) {
-            this.addOreToGenerate(Config.veinCountCopper, Config.veinSizeCopper, Config.minCopperY, Config.maxCopperY, BPBlocks.copper_ore, world,
+            this.addOreToGenerate(random, Config.veinCountCopper, Config.veinSizeCopper, Config.minCopperY, Config.maxCopperY, BPBlocks.copper_ore, world,
                     chunkX, chunkZ);
         }
         
@@ -95,14 +95,13 @@ public class WorldGenerationHandler implements IWorldGenerator {
         }
     }
     
-    private void addOreToGenerate(int veinCount, int veinSize, int minY, int maxY, Block block, World world, int chunkX, int chunkZ) {
+    private void addOreToGenerate(Random random, int veinCount, int veinSize, int minY, int maxY, Block block, World world, int chunkX, int chunkZ) {
     
-        Random rand = new Random(Integer.valueOf(chunkX).hashCode() + Integer.valueOf(chunkZ).hashCode());
         for (int i = 0; i < veinCount; i++) {
-            int x = chunkX * 16 + rand.nextInt(16);
-            int y = rand.nextInt(maxY - minY) + minY;
-            int z = chunkZ * 16 + rand.nextInt(16);
-            (new WorldGenMinable(block, veinSize)).generate(world, rand, x, y, z);
+            int x = chunkX * 16 + random.nextInt(16);
+            int y = random.nextInt(maxY - minY) + minY;
+            int z = chunkZ * 16 + random.nextInt(16);
+            (new WorldGenMinable(block, veinSize)).generate(world, random, x, y, z);
         }
     }
 }
