8e2b71a1c2efc2e19bb06e6613cccb2d82eabf2c
Quetzi <q@quetzi.net>
2014-06-11 13:19:23 +0100

    Creative Tab tidy up

--- src/main/java/net/quetzi/bluepower/init/CustomTabs.java
+++ src/main/java/net/quetzi/bluepower/init/CustomTabs.java
@@ -20,60 +20,58 @@ package net.quetzi.bluepower.init;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 
 public class CustomTabs {
-
+    
     public static CreativeTabs tabBluePowerBlocks;
     public static CreativeTabs tabBluePowerMachines;
     public static CreativeTabs tabBluePowerItems;
     public static CreativeTabs tabBluePowerTools;
     public static CreativeTabs tabBluePowerCircuits;
     public static CreativeTabs tabBluePowerLighting;
-
+    
     public static void init() {
-
+    
         tabBluePowerBlocks = new CreativeTabs("tabBluePowerBlocks") {
-
+            
             @Override
             public Item getTabIconItem() {
-
-                // Todo: Referer to a static object in the Blocks class.
-                Block iconBlock = BPBlocks.basalt;
+            
+                Block iconBlock = BPBlocks.marble;
                 if (iconBlock != null) {
                     return Item.getItemFromBlock(iconBlock);
                 } else {
-                    return Item.getItemFromBlock(net.minecraft.init.Blocks.stone);
+                    return Item.getItemFromBlock(Blocks.stone);
                 }
             }
         };
-
+        
         tabBluePowerMachines = new CreativeTabs("tabBluePowerMachines") {
-
+            
             @Override
             public Item getTabIconItem() {
-
-                // Todo: Referer to a static object in the Blocks class.
+            
                 Block iconBlock = BPBlocks.alloy_furnace;
                 if (iconBlock != null) {
                     return Item.getItemFromBlock(iconBlock);
                 } else {
-                    return Item.getItemFromBlock(net.minecraft.init.Blocks.furnace);
+                    return Item.getItemFromBlock(Blocks.furnace);
                 }
             }
         };
-
+        
         tabBluePowerItems = new CreativeTabs("tabBluePowerItems") {
-
+            
             @Override
             public Item getTabIconItem() {
-
-                // Todo: Referer to a static object in the Items class.
+            
                 Item iconItem = BPItems.ruby;
                 if (iconItem != null) {
-                    return net.quetzi.bluepower.init.BPItems.ruby;
+                    return BPItems.ruby;
                 } else {
-                    return net.minecraft.init.Items.diamond;
+                    return Items.diamond;
                 }
             }
         };
@@ -83,22 +81,22 @@ public class CustomTabs {
             @Override
             public Item getTabIconItem() {
 
-                // Todo: Referer to a static object in the Items class.
-                Item iconItem = BPItems.ruby_pickaxe;
+                Item iconItem = BPItems.screwdriver;
                 if (iconItem != null) {
-                    return net.quetzi.bluepower.init.BPItems.ruby_pickaxe;
+                    return BPItems.screwdriver;
                 } else {
-                    return net.minecraft.init.Items.diamond_pickaxe;
+                    return Items.diamond_pickaxe;
                 }
             }
         };
 
         tabBluePowerCircuits = new CreativeTabs("tabBluePowerCircuits") {
 
+            // TODO: Return a BP block icon
             @Override
             public Item getTabIconItem() {
 
-                return net.minecraft.init.Items.redstone;
+                return Items.redstone;
             }
         };
 
