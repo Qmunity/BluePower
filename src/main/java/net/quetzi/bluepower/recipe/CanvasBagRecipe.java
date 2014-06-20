437757b635a8e794741d0ffe2f031a10a45979d6
Lumien <lumien231@users.noreply.github.com>
2014-06-20 12:48:50 +0200

    Improve Canvas Bag Recipe

--- src/main/java/net/quetzi/bluepower/recipe/CanvasBagRecipe.java
+++ src/main/java/net/quetzi/bluepower/recipe/CanvasBagRecipe.java
@@ -19,126 +19,36 @@
 
 package net.quetzi.bluepower.recipe;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
-import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
-import net.quetzi.bluepower.init.BPItems;
-import net.quetzi.bluepower.references.Refs;
+import net.quetzi.bluepower.items.ItemCanvasBag;
 
 public class CanvasBagRecipe extends ShapelessOreRecipe {
     
-    public CanvasBagRecipe() {
+    public CanvasBagRecipe(ItemStack result, Object... recipe) {
     
-        super(BPItems.canvas_bag, new ItemStack(BPItems.canvas_bag, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Items.dye, 1,
-                OreDictionary.WILDCARD_VALUE));
-    }
-    
-    @SuppressWarnings("unchecked")
-    @Override
-    public boolean matches(InventoryCrafting var1, World world) {
-    
-        ArrayList<Object> required = new ArrayList<Object>(this.getInput());
-        
-        for (int x = 0; x < var1.getSizeInventory(); x++) {
-            ItemStack slot = var1.getStackInSlot(x);
-            
-            if (slot != null) {
-                boolean inRecipe = false;
-                Iterator<Object> req = required.iterator();
-                
-                while (req.hasNext()) {
-                    boolean match = false;
-                    
-                    Object next = req.next();
-                    
-                    if (next instanceof ItemStack) {
-                        int[] ids = OreDictionary.getOreIDs(slot);
-                        for (int id : ids) {
-                            String name = OreDictionary.getOreName(id);
-                            for (int d = 0; d < Refs.oreDictDyes.length; d++) {
-                                String dyeName = Refs.oreDictDyes[d];
-                                if (name.equals(dyeName)) {
-                                    match = true;
-                                }
-                            }
-                        }
-                        if (!match) {
-                            match = OreDictionary.itemMatches((ItemStack) next, slot, false);
-                        }
-                    } else if (next instanceof ArrayList) {
-                        Iterator<ItemStack> itr = ((ArrayList<ItemStack>) next).iterator();
-                        while (itr.hasNext() && !match) {
-                            int[] ids = OreDictionary.getOreIDs(slot);
-                            for (int id : ids) {
-                                String name = OreDictionary.getOreName(id);
-                                for (int d = 0; d < Refs.oreDictDyes.length; d++) {
-                                    String dyeName = Refs.oreDictDyes[d];
-                                    if (name.equals(dyeName)) {
-                                        match = true;
-                                    }
-                                }
-                            }
-                            if (!match) {
-                                match = OreDictionary.itemMatches(itr.next(), slot, false);
-                            }
-                        }
-                    }
-                    
-                    if (match) {
-                        inRecipe = true;
-                        required.remove(next);
-                        break;
-                    }
-                }
-                
-                if (!inRecipe) { return false; }
-            }
-        }
-        
-        return required.isEmpty();
+        super(result, recipe);
     }
     
     @Override
-    public ItemStack getCraftingResult(InventoryCrafting var1) {
+    public ItemStack getCraftingResult(InventoryCrafting inventoryCrafting) {
     
-        ItemStack dye = null;
-        ItemStack canvasBag = null;
-        for (int i = 0; i < var1.getSizeInventory(); i++) {
-            ItemStack is = var1.getStackInSlot(i);
-            if (is != null) {
-                if (is.getItem() == BPItems.canvas_bag) {
-                    canvasBag = is;
-                } else if (is != null) {
-                    dye = is;
-                }
+        ItemStack bag=null;
+        for (int slot=0;slot<inventoryCrafting.getSizeInventory();slot++)
+        {
+            if (inventoryCrafting.getStackInSlot(slot)!=null && inventoryCrafting.getStackInSlot(slot).getItem() instanceof ItemCanvasBag)
+            {
+                bag = inventoryCrafting.getStackInSlot(slot);
             }
         }
-        if (canvasBag == null) {
-        	return null;
-        }
-        if (dye != null) {
-            for (int i = 0; i < Refs.oreDictDyes.length; i++) {
-                int[] ids = OreDictionary.getOreIDs(dye);
-                for (int id : ids) {
-                    String name = OreDictionary.getOreName(id);
-                    for (int d = 0; d < Refs.oreDictDyes.length; d++) {
-                        String dyeName = Refs.oreDictDyes[d];
-                        if (name.equals(dyeName) && !(d == 15 - canvasBag.getItemDamage())) {
-                            ItemStack output = canvasBag.copy();
-                            output.setItemDamage(15 - d);
-                            return output;
-                        }
-                    }
-                }
-            }
+        if (bag!=null)
+        {
+            bag=bag.copy();
+            bag.setItemDamage(this.getRecipeOutput().getItemDamage());
+            return bag;
         }
         
-        return null;
+        return this.getRecipeOutput();
     }
 }
