d2e719bdd2aa2156738d8fc486df5f277a664bfd
amadornes <amadornes@gmail.com>
2014-06-20 20:22:49 +0200

    Gates: Pulse Former + Randomizer \o/

---
+++ src/main/java/net/quetzi/bluepower/part/gate/GatePulseFormer.java
@@ -0,0 +1,72 @@
+package net.quetzi.bluepower.part.gate;
+
+import java.util.List;
+
+import net.minecraft.util.AxisAlignedBB;
+import net.quetzi.bluepower.api.part.FaceDirection;
+import net.quetzi.bluepower.api.part.RedstoneConnection;
+import net.quetzi.bluepower.client.renderers.RenderHelper;
+
+public class GatePulseFormer extends GateBase {
+    
+    private boolean power[] = new boolean[4];
+    
+    @Override
+    public void initializeConnections(RedstoneConnection front, RedstoneConnection left, RedstoneConnection back, RedstoneConnection right) {
+    
+        // Init front
+        front.enable();
+        front.setOutput();
+        
+        // Init left
+        left.disable();
+        
+        // Init back
+        back.enable();
+        back.setInput();
+        
+        // Init right
+        right.disable();
+    }
+    
+    @Override
+    public String getGateID() {
+    
+        return "pulseformer";
+    }
+    
+    @Override
+    public void renderTop(RedstoneConnection front, RedstoneConnection left, RedstoneConnection back, RedstoneConnection right, float frame) {
+    
+        RenderHelper.renderRedstoneTorch(-3 / 16D, 1D / 8D, 1 / 16D, 9D / 16D, !power[0]);
+        RenderHelper.renderRedstoneTorch(3 / 16D, 1D / 8D, 1 / 16D, 9D / 16D, false);
+        RenderHelper.renderRedstoneTorch(0, 1D / 8D, -5 / 16D, 9D / 16D, !power[2] && power[1]);
+        
+        renderTopTexture(FaceDirection.BACK, power[0]);
+        renderTopTexture(FaceDirection.LEFT, !power[1]);
+        renderTopTexture(FaceDirection.RIGHT, power[2]);
+    }
+    
+    @Override
+    public void addOcclusionBoxes(List<AxisAlignedBB> boxes) {
+    
+        super.addOcclusionBoxes(boxes);
+    }
+    
+    @Override
+    public void doLogic(RedstoneConnection front, RedstoneConnection left, RedstoneConnection back, RedstoneConnection right) {
+    
+        power[3] = power[2];
+        power[2] = power[1];
+        power[1] = power[0];
+        power[0] = back.getPower() > 0;
+        
+        if (power[2] && !power[3]) {
+            front.setPower(15);
+        } else {
+            front.setPower(0);
+        }
+        
+    }
+    
+}
---
+++ src/main/java/net/quetzi/bluepower/part/gate/GateRandomizer.java
@@ -0,0 +1,106 @@
+package net.quetzi.bluepower.part.gate;
+
+import java.util.List;
+import java.util.Random;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.AxisAlignedBB;
+import net.quetzi.bluepower.api.part.FaceDirection;
+import net.quetzi.bluepower.api.part.RedstoneConnection;
+import net.quetzi.bluepower.client.renderers.RenderHelper;
+import net.quetzi.bluepower.references.Refs;
+
+public class GateRandomizer extends GateBase {
+    
+    private static final Random random = new Random();
+    
+    private int                 ticks  = 0;
+    
+    private boolean             out[]  = new boolean[3];
+    
+    @Override
+    public void initializeConnections(RedstoneConnection front, RedstoneConnection left, RedstoneConnection back, RedstoneConnection right) {
+    
+        // Init front
+        front.enable();
+        front.setOutput();
+        
+        // Init left
+        left.enable();
+        left.setOutput();
+        
+        // Init back
+        back.enable();
+        back.setInput();
+        
+        // Init right
+        right.enable();
+        right.setOutput();
+    }
+    
+    @Override
+    public String getGateID() {
+    
+        return "randomizer";
+    }
+    
+    @Override
+    public void renderTop(RedstoneConnection front, RedstoneConnection left, RedstoneConnection back, RedstoneConnection right, float frame) {
+    
+        renderTopTexture(FaceDirection.LEFT, out[0]);
+        renderTopTexture(FaceDirection.FRONT, out[1]);
+        renderTopTexture(FaceDirection.RIGHT, out[2]);
+        renderTopTexture(Refs.MODID + ":textures/blocks/gates/" + getType() + "/center_" + (back.getPower() > 0 ? "on" : "off") + ".png");
+        
+        RenderHelper.renderRandomizerButton(this, -4 / 16D, 0, 6 / 16D, out[0]);
+        RenderHelper.renderRandomizerButton(this, 0, 0, 0, out[1]);
+        RenderHelper.renderRandomizerButton(this, 4 / 16D, 0, 6 / 16D, out[2]);
+    }
+    
+    @Override
+    public void addOcclusionBoxes(List<AxisAlignedBB> boxes) {
+    
+        super.addOcclusionBoxes(boxes);
+        
+        boxes.add(AxisAlignedBB.getBoundingBox(7D / 16D, 2D / 16D, 7D / 16D, 9D / 16D, 8D / 16D, 9D / 16D));
+    }
+    
+    @Override
+    public void doLogic(RedstoneConnection front, RedstoneConnection left, RedstoneConnection back, RedstoneConnection right) {
+    
+        if (!world.isRemote) {
+            if (back.getPower() > 0) {
+                if (ticks % 10 == 0) {
+                    out[0] = random.nextBoolean();
+                    out[1] = random.nextBoolean();
+                    out[2] = random.nextBoolean();
+                    sendUpdatePacket();
+                }
+                ticks++;
+            } else {
+                ticks = 0;
+            }
+            left.setPower(out[0] ? 15 : 0);
+            front.setPower(out[1] ? 15 : 0);
+            right.setPower(out[2] ? 15 : 0);
+        }
+    }
+    
+    @Override
+    public void save(NBTTagCompound tag) {
+    
+        super.save(tag);
+        tag.setBoolean("out_0", out[0]);
+        tag.setBoolean("out_1", out[1]);
+        tag.setBoolean("out_2", out[2]);
+    }
+    
+    @Override
+    public void load(NBTTagCompound tag) {
+    
+        super.load(tag);
+        out[0] = tag.getBoolean("out_0");
+        out[1] = tag.getBoolean("out_1");
+        out[2] = tag.getBoolean("out_2");
+    }
+}
