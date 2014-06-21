78f76ee6571016f2958028fb0423d10403afffef
Quetzi <q@quetzi.net>
2014-06-21 18:05:32 +0100

    Texures for Relay & Ejector

--- src/main/java/net/quetzi/bluepower/blocks/machines/BlockRelay.java
+++ src/main/java/net/quetzi/bluepower/blocks/machines/BlockRelay.java
@@ -17,8 +17,13 @@
 
 package net.quetzi.bluepower.blocks.machines;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraftforge.common.util.ForgeDirection;
 import net.quetzi.bluepower.blocks.BlockContainerBase;
 import net.quetzi.bluepower.init.CustomTabs;
 import net.quetzi.bluepower.references.GuiIDs;
@@ -26,6 +31,13 @@ import net.quetzi.bluepower.references.Refs;
 import net.quetzi.bluepower.tileentities.tier1.TileRelay;
 
 public class BlockRelay extends BlockContainerBase {
+
+    private IIcon textureFront;
+    private IIcon textureBack;
+    private IIcon textureSide1;
+    private IIcon textureSide2;
+    private IIcon textureSide2Active;
+    public boolean isActive = false;
 
     public BlockRelay() {
 
@@ -52,5 +64,32 @@ public class BlockRelay extends BlockContainerBase {
     @Override public GuiIDs getGuiID() {
 
         return GuiIDs.RELAY_ID;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister) {
+
+        this.textureFront = iconRegister.registerIcon(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.RELAY_NAME + "_front");
+        this.textureBack = iconRegister.registerIcon(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.RELAY_NAME + "_back");
+        this.textureSide1 = iconRegister.registerIcon(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.RELAY_NAME + "_side_0");
+        this.textureSide2 = iconRegister.registerIcon(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.RELAY_NAME + "_side_1");
+        this.textureSide2Active = iconRegister.registerIcon(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.RELAY_NAME + "_side_1_active");
+        this.blockIcon = this.textureSide1;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta) {
+
+        ForgeDirection direction = ForgeDirection.getOrientation(meta);
+        if (side == direction.ordinal()) {
+            return textureFront;
+        } else if (side == direction.getOpposite().ordinal()) {
+            return textureBack;
+        } if ((side == 3) || (side == 2)) {
+            return isActive ? textureSide2Active : textureSide2;
+        }
+        return blockIcon;
     }
 }
