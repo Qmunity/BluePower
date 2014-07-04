6def7d0aa454a6bf147489c185b0d481d4e7248e
Quetzi <q@quetzi.net>
2014-07-04 17:54:45 +0100

    Disable Disjunction enchant

--- src/main/java/net/quetzi/bluepower/enchantments/EnchantmentDisjunction.java
+++ src/main/java/net/quetzi/bluepower/enchantments/EnchantmentDisjunction.java
@@ -27,12 +27,12 @@ public class EnchantmentDisjunction extends Enchantment {
     public int getMaxEnchantability(int par1) {
         return this.getMinEnchantability(par1) + 20;
     }
-	
-	@SuppressWarnings("cast")
-	@Override
-	public float calcModifierLiving(int par1, EntityLivingBase entity) {
-		return entity instanceof EntityEnderman ? (float)par1 * 2.5F : (entity instanceof EntitySkeleton ? (((EntitySkeleton) entity).getSkeletonType() == 1 ? (float)par1 * 2.5F : 0.0F) : 0.0F);
-	}
+	// TODO: Broken on 1.7.10 update
+//	@SuppressWarnings("cast")
+//	@Override
+//	public float calcModifierLiving(int par1, EntityLivingBase entity) {
+//		return entity instanceof EntityEnderman ? (float)par1 * 2.5F : (entity instanceof EntitySkeleton ? (((EntitySkeleton) entity).getSkeletonType() == 1 ? (float)par1 * 2.5F : 0.0F) : 0.0F);
+//	}
 	
 	@Override
 	public String getTranslatedName(int level) {
