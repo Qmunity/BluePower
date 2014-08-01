85eeddac8ddc200fee771c9336842d5dd8c4af34
MineMaarten <loopycoaster_8@hotmail.com>
2014-08-01 13:04:47 +0200

    Fixed Sorting Machine container, and fixed and finished up the Ejector

--- src/main/java/com/bluepowermod/containers/ContainerEjector.java
+++ src/main/java/com/bluepowermod/containers/ContainerEjector.java
@@ -24,53 +24,55 @@ import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+
 import com.bluepowermod.tileentities.tier1.TileEjector;
 
 public class ContainerEjector extends Container {
-
+    
     private final TileEjector tileEjector;
-
+    
     public ContainerEjector(InventoryPlayer invPlayer, TileEjector ejector) {
-
-        this.tileEjector = ejector;
-
+    
+        tileEjector = ejector;
+        
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
-                this.addSlotToContainer(new Slot(ejector, j + i * 3, 62 + j * 18, 17 + i * 18));
+                addSlotToContainer(new Slot(ejector, j + i * 3, 62 + j * 18, 17 + i * 18));
             }
         }
         bindPlayerInventory(invPlayer);
     }
-
+    
     protected void bindPlayerInventory(InventoryPlayer invPlayer) {
-
+    
         // Render inventory
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 9; j++) {
                 addSlotToContainer(new Slot(invPlayer, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
             }
         }
-
+        
         // Render hotbar
         for (int j = 0; j < 9; j++) {
             addSlotToContainer(new Slot(invPlayer, j, 8 + j * 18, 142));
         }
     }
-
-    @Override public boolean canInteractWith(EntityPlayer player) {
-
+    
+    @Override
+    public boolean canInteractWith(EntityPlayer player) {
+    
         return tileEjector.isUseableByPlayer(player);
     }
-
+    
     @Override
     public ItemStack transferStackInSlot(EntityPlayer player, int par2) {
-
+    
         ItemStack itemstack = null;
         Slot slot = (Slot) inventorySlots.get(par2);
         if (slot != null && slot.getHasStack()) {
             ItemStack itemstack1 = slot.getStack();
             itemstack = itemstack1.copy();
-            if (par2 < 20) {
+            if (par2 < 9) {
                 if (!mergeItemStack(itemstack1, 9, 45, true)) return null;
             } else if (!mergeItemStack(itemstack1, 0, 9, false)) { return null; }
             if (itemstack1.stackSize == 0) {
--- src/main/java/com/bluepowermod/containers/ContainerSortingMachine.java
+++ src/main/java/com/bluepowermod/containers/ContainerSortingMachine.java
@@ -77,33 +77,7 @@ public class ContainerSortingMachine extends Container {
     @Override
     public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int par2) {
     
-        ItemStack var3 = null;
-        Slot var4 = (Slot) inventorySlots.get(par2);
-        
-        if (var4 != null && var4.getHasStack()) {
-            ItemStack var5 = var4.getStack();
-            var3 = var5.copy();
-            
-            if (par2 < 40) {
-                if (!mergeItemStack(var5, 40, 76, false)) return null;
-                var4.onSlotChange(var5, var3);
-            } else {
-                if (!mergeItemStack(var5, 0, 40, false)) return null;
-                var4.onSlotChange(var5, var3);
-            }
-            
-            if (var5.stackSize == 0) {
-                var4.putStack((ItemStack) null);
-            } else {
-                var4.onSlotChanged();
-            }
-            
-            if (var5.stackSize == var3.stackSize) return null;
-            
-            var4.onPickupFromSlot(par1EntityPlayer, var5);
-        }
-        
-        return var3;
+        return null;
     }
     
     /**
