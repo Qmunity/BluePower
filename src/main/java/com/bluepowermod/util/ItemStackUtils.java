fcf82888aaa238ddbdfe2e62507d48aa742a66d1
MineMaarten <loopycoaster_8@hotmail.com>
2014-08-03 17:23:58 +0200

    Teached the Alloy Furnace some words out of the OreDict it had forgotten

--- src/main/java/com/bluepowermod/util/ItemStackUtils.java
+++ src/main/java/com/bluepowermod/util/ItemStackUtils.java
@@ -31,19 +31,19 @@ public class ItemStackUtils {
     
     public static boolean isItemFuzzyEqual(ItemStack stack1, ItemStack stack2) {
     
-        if (stack1.getItem() != stack2.getItem() && !isSameOreDictStack(stack1, stack2)) return false;
+        if (isSameOreDictStack(stack1, stack2)) { return true; }
+        if (stack1.getItem() != stack2.getItem()) return false;
         return stack1.getItemDamage() == stack2.getItemDamage() || stack1.getItemDamage() == OreDictionary.WILDCARD_VALUE || stack2.getItemDamage() == OreDictionary.WILDCARD_VALUE;
     }
     
     public static boolean isSameOreDictStack(ItemStack stack1, ItemStack stack2) {
     
         int ids[] = OreDictionary.getOreIDs(stack1);
-        for (int id = 0; id < ids.length; id++) {
-            if (id >= 0) {
-                List<ItemStack> oreDictStacks = OreDictionary.getOres(OreDictionary.getOreName(id));
-                for (ItemStack oreDictStack : oreDictStacks) {
-                    if (OreDictionary.itemMatches(oreDictStack, stack2, false)) { return true; }
-                }
+        for (int id : ids) {
+            String name = OreDictionary.getOreName(id);
+            List<ItemStack> oreDictStacks = OreDictionary.getOres(name);
+            for (ItemStack oreDictStack : oreDictStacks) {
+                if (OreDictionary.itemMatches(stack2, oreDictStack, false)) { return true; }
             }
         }
         return false;
