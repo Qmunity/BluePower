92dc355dc3c16297657a88f5b16b483486705aa4
MineMaarten <loopycoaster_8@hotmail.com>
2014-08-06 15:16:00 +0200

    Added no FMP warning

--- src/main/java/com/bluepowermod/events/BPEventHandler.java
+++ src/main/java/com/bluepowermod/events/BPEventHandler.java
@@ -11,7 +11,9 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntityHopper;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EntityDamageSource;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
@@ -20,15 +22,30 @@ import com.bluepowermod.containers.ContainerSeedBag;
 import com.bluepowermod.containers.inventorys.InventoryItem;
 import com.bluepowermod.init.BPEnchantments;
 import com.bluepowermod.items.ItemSeedBag;
+import com.bluepowermod.util.Dependencies;
 
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 
 public class BPEventHandler {
-
+    
+    private boolean warned = false;
+    
+    @SubscribeEvent
+    public void onTick(TickEvent.PlayerTickEvent event) {
+    
+        if (event.phase == TickEvent.Phase.END && event.player.worldObj.isRemote && !warned && !Loader.isModLoaded(Dependencies.FMP)) {
+            event.player.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.RED + "WARNING: You're running BluePower without having " + EnumChatFormatting.RED + "ForgeMultipart installed. Even though it will work, " + EnumChatFormatting.RED + "ForgeMultipart is "
+                    + EnumChatFormatting.BOLD + "highly" + EnumChatFormatting.RESET + EnumChatFormatting.RED + " recommended. Continue at own risk, " + EnumChatFormatting.RED + "expect bugs."));
+            warned = true;
+        }
+    }
+    
     @SubscribeEvent
     public void itemPickUp(EntityItemPickupEvent event) {
-
+    
         EntityPlayer player = event.entityPlayer;
         ItemStack pickUp = event.item.getEntityItem();
         if (!(player.openContainer instanceof ContainerSeedBag)) {
@@ -40,7 +57,7 @@ public class BPEventHandler {
                         inventory.openInventory();
                         ItemStack pickedUp = TileEntityHopper.func_145889_a(inventory, pickUp, -1);
                         inventory.closeInventory(is);
-
+                        
                         if (pickedUp == null) {
                             event.setResult(Result.ALLOW);
                             event.item.setDead();
@@ -53,19 +70,19 @@ public class BPEventHandler {
             }
         }
     }
-
+    
     private boolean isAttacking = false;
-
+    
     @SubscribeEvent
     public void onEntityAttack(LivingAttackEvent event) {
-
+    
         if (!isAttacking && event.source instanceof EntityDamageSource) {// this event will be trigger recursively by EntityLiving#attackEntityFrom,
                                                                          // so we need to stop the loop.
             EntityDamageSource entitySource = (EntityDamageSource) event.source;
-
+            
             if (entitySource.getEntity() instanceof EntityPlayer) {
                 EntityPlayer killer = (EntityPlayer) entitySource.getEntity();
-
+                
                 if (killer.inventory.getCurrentItem() != null) {
                     if (EnchantmentHelper.getEnchantments(killer.inventory.getCurrentItem()).containsKey(BPEnchantments.disjunction.effectId)) {
                         if (event.entityLiving instanceof EntityEnderman || event.entityLiving instanceof EntityDragon) {
@@ -79,22 +96,22 @@ public class BPEventHandler {
                 }
             }
         }
-
+        
     }
-
+    
     @SubscribeEvent
     public void onEntityDeath(LivingDeathEvent event) {
-
+    
         if (event.source instanceof EntityDamageSource) {
             EntityDamageSource entitySource = (EntityDamageSource) event.source;
-
+            
             if (entitySource.getEntity() instanceof EntityPlayer) {
                 EntityPlayer killer = (EntityPlayer) entitySource.getEntity();
-
+                
                 if (killer.inventory.getCurrentItem() != null) {
                     if (EnchantmentHelper.getEnchantments(killer.inventory.getCurrentItem()).containsKey(BPEnchantments.vorpal.effectId)) {
                         int level = EnchantmentHelper.getEnchantmentLevel(BPEnchantments.vorpal.effectId, killer.inventory.getCurrentItem());
-
+                        
                         if (level == 1) {
                             if (killer.worldObj.rand.nextInt(6) == 1) {
                                 dropHeads(event);
@@ -109,14 +126,14 @@ public class BPEventHandler {
             }
         }
     }
-
+    
     private void dropHeads(LivingDeathEvent event) {
-
+    
         if (event.entityLiving instanceof EntityCreeper) {
             event.entityLiving.entityDropItem(new ItemStack(Items.skull, 1, 4), 0.0F);
             return;
         }
-
+        
         if (event.entityLiving instanceof EntityPlayer) {
             ItemStack drop = new ItemStack(Items.skull, 1, 3);
             drop.stackTagCompound = new NBTTagCompound();
@@ -124,10 +141,10 @@ public class BPEventHandler {
             event.entityLiving.entityDropItem(drop, 0.0F);
             return;
         }
-
+        
         if (event.entityLiving instanceof EntitySkeleton) {
             EntitySkeleton sk = (EntitySkeleton) event.entityLiving;
-
+            
             if (sk.getSkeletonType() == 0) {
                 event.entityLiving.entityDropItem(new ItemStack(Items.skull, 1, 0), 0.0F);
                 return;
@@ -136,7 +153,7 @@ public class BPEventHandler {
                 return;
             }
         }
-
+        
         if (event.entityLiving instanceof EntityZombie) {
             event.entityLiving.entityDropItem(new ItemStack(Items.skull, 1, 2), 0.0F);
             return;
