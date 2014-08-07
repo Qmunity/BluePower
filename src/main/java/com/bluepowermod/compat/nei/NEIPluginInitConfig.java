58925c4056fc2aa4639cb1fa87c4f3e640d56287
MineMaarten <loopycoaster_8@hotmail.com>
2014-08-07 15:02:22 +0200

    Turned off debug option

--- src/main/java/com/bluepowermod/compat/nei/NEIPluginInitConfig.java
+++ src/main/java/com/bluepowermod/compat/nei/NEIPluginInitConfig.java
@@ -2,7 +2,6 @@ package com.bluepowermod.compat.nei;
 
 import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
-import codechicken.nei.guihook.GuiContainerManager;
 
 import com.bluepowermod.client.gui.GuiProjectTable;
 import com.bluepowermod.util.Refs;
@@ -20,7 +19,7 @@ public class NEIPluginInitConfig implements IConfigureNEI {
         API.registerUsageHandler(new AlloyFurnaceHandler());
         API.registerRecipeHandler(new AlloyFurnaceHandler());
         API.registerGuiOverlayHandler(GuiProjectTable.class, new ProjectTableOverlayHandler(), "crafting");
-        GuiContainerManager.addTooltipHandler(new DebugTooltipHandler());
+        //  GuiContainerManager.addTooltipHandler(new DebugTooltipHandler());//Remove comments to show the GameData name of an item when hovering over it.
     }
     
     @Override
