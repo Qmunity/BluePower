98e0b4fcf5611ea58e4c024689b3a08df7fd729e
Amadornes <amadornes@gmail.com>
2014-08-08 21:49:21 +0200

    Block lamps now render their halo on pass 1

--- src/main/java/com/bluepowermod/blocks/machines/BlockLamp.java
+++ src/main/java/com/bluepowermod/blocks/machines/BlockLamp.java
@@ -18,37 +18,37 @@ import cpw.mods.fml.relauncher.SideOnly;
  * 
  */
 public class BlockLamp extends BlockContainerBase {
-    
-    private boolean      isInverted;
+
+    private boolean isInverted;
     private final String colorName;
-    private int          color;
-    
+    private int color;
+
     public BlockLamp(boolean _isInverted, String _colorName, int _color) {
-    
+
         super(Material.iron, TileLamp.class);
         setInverted(_isInverted);
         colorName = _colorName;
         setColor(_color);
         setBlockName(Refs.LAMP_NAME + (isInverted ? "inverted" : "") + colorName);
         setCreativeTab(CustomTabs.tabBluePowerLighting);
-        
+
     }
-    
+
     @Override
     @SideOnly(Side.CLIENT)
     public void registerBlockIcons(IIconRegister iconRegister) {
-    
+
         blockIcon = iconRegister.registerIcon(Refs.MODID + ":lamps/lamp_on");
         blockIcon = iconRegister.registerIcon(Refs.MODID + ":lamps/lamp_off");
     }
-    
+
     @Override
     public int getLightValue(IBlockAccess w, int x, int y, int z) {
-    
+
         TileLamp tileEntity = (TileLamp) w.getTileEntity(x, y, z);
         if (tileEntity != null) {
             int power = tileEntity.getPower();
-            
+
             if (isInverted()) {
                 power = 15 - power;
             }
@@ -57,46 +57,51 @@ public class BlockLamp extends BlockContainerBase {
             return 0;
         }
     }
-    
+
     @Override
     @SideOnly(Side.CLIENT)
     public int getRenderType() {
-    
+
         return RenderLamp.RENDER_ID;
     }
-    
+
     public int getColor() {
-    
+
         return color;
     }
-    
+
     public void setColor(int color) {
-    
+
         this.color = color;
     }
-    
+
     public boolean isInverted() {
-    
+
         return isInverted;
     }
-    
+
     public void setInverted(boolean isInverted) {
-    
+
         this.isInverted = isInverted;
     }
 
-    public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side){
+    @Override
+    public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side) {
 
         return true;
     }
 
     @Override
-    public boolean canRenderInPass(int pass){
+    public boolean canRenderInPass(int pass) {
+
+        RenderLamp.pass = pass;
+
         return true;
     }
 
     @Override
-    public int getRenderBlockPass(){
+    public int getRenderBlockPass() {
+
         return 1;
     }
 }
--- src/main/java/com/bluepowermod/client/renderers/RenderLamp.java
+++ src/main/java/com/bluepowermod/client/renderers/RenderLamp.java
@@ -8,7 +8,6 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 
-import net.minecraftforge.client.MinecraftForgeClient;
 import org.lwjgl.opengl.GL11;
 
 import com.bluepowermod.api.vec.Vector3Cube;
@@ -19,78 +18,79 @@ import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class RenderLamp extends TileEntitySpecialRenderer implements ISimpleBlockRenderingHandler {
-    
+
     public static final int RENDER_ID = RenderingRegistry.getNextAvailableRenderId();
-    
+
+    public static int pass;
+
     @Override
     public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
-    
+
         BlockLamp bLamp = (BlockLamp) block;
         int redMask = 0xFF0000, greenMask = 0xFF00, blueMask = 0xFF;
         int r = (bLamp.getColor() & redMask) >> 16;
         int g = (bLamp.getColor() & greenMask) >> 8;
         int b = (bLamp.getColor() & blueMask);
         Vector3Cube vector = new Vector3Cube(0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
-        
-        // if (pass == 0) {
-        Tessellator t = Tessellator.instance;
-        t.startDrawingQuads();
-        t.setColorOpaque(r, g, b);
-        IIcon iconToUse;
-        int power = 0;
-        if (power == 0) {
-            iconToUse = IconSupplier.lampOff;
-        } else {
-            iconToUse = IconSupplier.lampOn;
+
+        if (pass == 0) {
+            Tessellator t = Tessellator.instance;
+            t.startDrawingQuads();
+            t.setColorOpaque(r, g, b);
+            IIcon iconToUse;
+            int power = 0;
+            if (power == 0) {
+                iconToUse = IconSupplier.lampOff;
+            } else {
+                iconToUse = IconSupplier.lampOn;
+            }
+
+            double minU = iconToUse.getMinU();
+            double maxU = iconToUse.getMaxU();
+            double minV = iconToUse.getMinV();
+            double maxV = iconToUse.getMaxV();
+
+            // Top side
+            t.setNormal(0, 1, 0);
+            t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMaxZ(), minU, maxV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMaxZ(), minU, minV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMinZ(), maxU, minV);
+            t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMinZ(), maxU, maxV);
+
+            // Draw west side:
+            t.setNormal(-1, 0, 0);
+            t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMaxZ(), minU, maxV);
+            t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMaxZ(), minU, minV);
+            t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMinZ(), maxU, minV);
+            t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMinZ(), maxU, maxV);
+
+            // Draw east side:
+            t.setNormal(1, 0, 0);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMinZ(), minU, maxV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMinZ(), minU, minV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMaxZ(), maxU, minV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMaxZ(), maxU, maxV);
+
+            // Draw north side
+            t.setNormal(0, 0, -1);
+            t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMinZ(), minU, maxV);
+            t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMinZ(), minU, minV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMinZ(), maxU, minV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMinZ(), maxU, maxV);
+
+            // Draw south side
+            t.setNormal(0, 0, 1);
+            t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMaxZ(), minU, maxV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMaxZ(), maxU, maxV);
+            t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMaxZ(), maxU, minV);
+            t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMaxZ(), minU, minV);
+
+            t.draw();
         }
-        
-        double minU = iconToUse.getMinU();
-        double maxU = iconToUse.getMaxU();
-        double minV = iconToUse.getMinV();
-        double maxV = iconToUse.getMaxV();
-        
-        // Top side
-        t.setNormal(0, 1, 0);
-        t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMaxZ(), minU, maxV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMaxZ(), minU, minV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMinZ(), maxU, minV);
-        t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMinZ(), maxU, maxV);
-        
-        // Draw west side:
-        t.setNormal(-1, 0, 0);
-        t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMaxZ(), minU, maxV);
-        t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMaxZ(), minU, minV);
-        t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMinZ(), maxU, minV);
-        t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMinZ(), maxU, maxV);
-        
-        // Draw east side:
-        t.setNormal(1, 0, 0);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMinZ(), minU, maxV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMinZ(), minU, minV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMaxZ(), maxU, minV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMaxZ(), maxU, maxV);
-        
-        // Draw north side
-        t.setNormal(0, 0, -1);
-        t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMinZ(), minU, maxV);
-        t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMinZ(), minU, minV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMinZ(), maxU, minV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMinZ(), maxU, maxV);
-        
-        // Draw south side
-        t.setNormal(0, 0, 1);
-        t.addVertexWithUV(vector.getMinX(), vector.getMinY(), vector.getMaxZ(), minU, maxV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMinY(), vector.getMaxZ(), maxU, maxV);
-        t.addVertexWithUV(vector.getMaxX(), vector.getMaxY(), vector.getMaxZ(), maxU, minV);
-        t.addVertexWithUV(vector.getMinX(), vector.getMaxY(), vector.getMaxZ(), minU, minV);
-        
-        t.draw();
-        // }
     }
-    
+
     @Override
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
-
 
         BlockLamp bLamp = (BlockLamp) block;
         int redMask = 0xFF0000, greenMask = 0xFF00, blueMask = 0xFF;
@@ -171,24 +171,24 @@ public class RenderLamp extends TileEntitySpecialRenderer implements ISimpleBloc
         t.addTranslation(-x, -y, -z);
         return true;
     }
-    
+
     @Override
     public boolean shouldRender3DInInventory(int modelId) {
-    
+
         return true;
     }
-    
+
     @Override
     public int getRenderId() {
-    
+
         return RENDER_ID;
     }
-    
+
     /******* TESR ***********/
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float f) {
 
-        if(MinecraftForgeClient.getRenderPass() == 1){
+        if (pass != 0) {
             int power = ((TileLamp) te).getPower();
 
             BlockLamp bLamp = (BlockLamp) te.getBlockType();
@@ -225,5 +225,5 @@ public class RenderLamp extends TileEntitySpecialRenderer implements ISimpleBloc
             GL11.glTranslated(-x, -y, -z);
         }
     }
-    
+
 }
