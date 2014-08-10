a19937644dd133dfe2c2150f864bf6d005bd9c60
MineMaarten <loopycoaster_8@hotmail.com>
2014-08-10 22:37:36 +0200

    Texture changes

--- src/main/java/com/bluepowermod/blocks/machines/BlockIgniter.java
+++ src/main/java/com/bluepowermod/blocks/machines/BlockIgniter.java
@@ -23,6 +23,7 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import com.bluepowermod.blocks.BlockContainerBase;
 import com.bluepowermod.client.renderers.RendererBlockBase.EnumFaceType;
 import com.bluepowermod.tileentities.tier1.TileIgniter;
 import com.bluepowermod.util.Refs;
@@ -30,44 +31,43 @@ import com.bluepowermod.util.Refs;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockIgniter extends BlockContainerTwoSideRender {
-
+public class BlockIgniter extends BlockContainerBase {
+    
     public BlockIgniter() {
-
+    
         super(Material.rock, TileIgniter.class);
         setBlockName(Refs.BLOCKIGNITER_NAME);
     }
-
+    
     @Override
     @SideOnly(Side.CLIENT)
     public void registerBlockIcons(IIconRegister iconRegister) {
-
+    
         super.registerBlockIcons(iconRegister);
         blockIcon = iconRegister.registerIcon(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKIGNITER_NAME + "_side_0");
     }
-
+    
     @Override
     protected String getIconName(EnumFaceType faceType, boolean ejecting, boolean powered) {
-
+    
         String iconName = textureName + "_" + faceType.toString().toLowerCase();
         if (faceType == EnumFaceType.FRONT) {
-            if (ejecting)
-                iconName += "_active";
+            if (ejecting) iconName += "_active";
         }
         return iconName;
     }
-
+    
     @Override
     public boolean isFireSource(World world, int x, int y, int z, ForgeDirection side) {
-
+    
         TileIgniter tile = (TileIgniter) world.getTileEntity(x, y, z);
         boolean orientation = tile.getFacingDirection() == ForgeDirection.UP;
         return orientation && tile.getIsRedstonePowered();
     }
-
+    
     @Override
     public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side) {
-
+    
         return true;
     }
 }
