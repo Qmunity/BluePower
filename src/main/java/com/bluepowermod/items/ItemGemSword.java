b22a005cbe7c310ee42d8c57b60ea8d155df9044
Quetzi <q@quetzi.net>
2014-09-06 11:55:39 +0100

    Custom tools are now repairable in anvils

--- src/main/java/com/bluepowermod/items/ItemGemSword.java
+++ src/main/java/com/bluepowermod/items/ItemGemSword.java
@@ -17,20 +17,24 @@
 
 package com.bluepowermod.items;
 
+import com.bluepowermod.init.CustomTabs;
+import com.bluepowermod.util.Refs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 
-import com.bluepowermod.init.CustomTabs;
-import com.bluepowermod.util.Refs;
-
 public class ItemGemSword extends ItemSword {
 
-    public ItemGemSword(ToolMaterial material, String name) {
+    public    Item    customCraftingMaterial = null;
+    protected boolean canRepair              = true;
+
+    public ItemGemSword(ToolMaterial material, String name, Item repairItem) {
 
         super(material);
         this.setUnlocalizedName(name);
         this.setCreativeTab(CustomTabs.tabBluePowerTools);
         this.setTextureName(Refs.MODID + ":" + name);
+        this.customCraftingMaterial = repairItem;
     }
 
     @Override
@@ -42,5 +46,17 @@ public class ItemGemSword extends ItemSword {
     protected String getUnwrappedUnlocalizedName(String name) {
 
         return name.substring(name.indexOf(".") + 1);
+    }
+
+    @Override
+    public boolean isRepairable() {
+
+        return canRepair && isDamageable();
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack is1, ItemStack is2) {
+
+        return ((is1.getItem() == this || is2.getItem() == this) && (is1.getItem() == this.customCraftingMaterial || is2.getItem() == this.customCraftingMaterial));
     }
 }
