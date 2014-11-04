3dbc62faf89a8c020d2ede9d170e8fdf7eebe143
Quetzi <q@quetzi.net>
2014-11-04 12:27:09 +0000

    Add Sequencer

---
+++ src/main/java/com/bluepowermod/part/gate/GateSequencer.java
@@ -0,0 +1,136 @@
+/*
+ * This file is part of Blue Power.
+ *
+ *      Blue Power is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      Blue Power is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with Blue Power.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.bluepowermod.part.gate;
+
+import com.bluepowermod.client.gui.gate.GuiGateSingleTime;
+import com.bluepowermod.client.renderers.RenderHelper;
+import com.bluepowermod.part.IGuiButtonSensitive;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.Arrays;
+
+/**
+ * Created by Quetzi on 04/11/14.
+ */
+public class GateSequencer extends GateBase implements IGuiButtonSensitive {
+
+    private final boolean[] power = new boolean[4];
+    private int             start = -1;
+    private int             time  = 160;
+    private int             ticks = 0;
+
+    @Override
+    public void initializeConnections() {
+
+        front().enable().setOutputOnly();
+        left().enable().setOutputOnly();
+        back().enable().setOutputOnly();
+        right().enable().setOutputOnly();
+    }
+
+    @Override
+    public String getId() {
+
+        return "sequencer";
+    }
+
+    @Override
+    public void renderTop(float frame) {
+
+        RenderHelper.renderRedstoneTorch(0, 1D / 8D, -5D / 16D, 8D / 16D, power[1]);
+        RenderHelper.renderRedstoneTorch(5D / 16D, 1D / 8D, 0, 8D / 16D, power[2]);
+        RenderHelper.renderRedstoneTorch(0, 1D / 8D, 5D / 16D, 8D / 16D, power[3]);
+        RenderHelper.renderRedstoneTorch(-5D / 16D, 1D / 8D, 0, 8D / 16D, power[0]);
+
+        RenderHelper.renderRedstoneTorch(0, 1D / 8D, 0, 13D / 16D, true);
+        RenderHelper.renderPointer(0, 7D / 16D, 0, getWorld() != null ? start >= 0 ? 1 - (double) (ticks - start + frame) / (double) time : 0 : 0);
+    }
+
+    @Override
+    public void doLogic() {
+
+    }
+
+    @Override
+    public void tick() {
+
+        if (!getWorld().isRemote && getWorld().getWorldTime() % 400 == 0) sendUpdatePacket();//Prevent slow desyncing of the timer.
+        Arrays.fill(power, false);
+
+        if (start >= 0) {
+            if (ticks >= start + time) {
+                start = ticks;
+            }
+        } else {
+            start = ticks;
+        }
+
+        double t = (double) (ticks - start) / (double) time;
+
+        if (t >= 1D / 8D && t < 3D / 8D) {
+            power[2] = true;
+            if (right().getOutput() == 0) playTickSound();
+        } else if (t >= 3D / 8D && t < 5D / 8D) {
+            power[3] = true;
+            if (back().getOutput() == 0) playTickSound();
+        } else if (t >= 5D / 8D && t < 7D / 8D) {
+            power[0] = true;
+            if (left().getOutput() == 0) playTickSound();
+        } else if (t >= 7D / 8D && t < 1 || t >= 0 && t < 1D / 8D) {
+            power[1] = true;
+            if (front().getOutput() == 0) playTickSound();
+        }
+
+        left().setOutput(power[0] ? 15 : 0);
+        front().setOutput(power[1] ? 15 : 0);
+        right().setOutput(power[2] ? 15 : 0);
+        back().setOutput(power[3] ? 15 : 0);
+
+        ticks++;
+    }
+
+    @Override
+    public void onButtonPress(EntityPlayer player, int messageId, int value) {
+
+        time = value * 4;
+        sendUpdatePacket();
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public GuiScreen getGui() {
+
+        return new GuiGateSingleTime(this) {
+
+            @Override
+            protected int getCurrentIntervalTicks() {
+
+                return time / 4;
+            }
+        };
+    }
+
+    @Override
+    protected boolean hasGUI() {
+
+        return true;
+    }
+}
