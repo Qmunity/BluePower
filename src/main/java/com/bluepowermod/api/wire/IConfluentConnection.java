2197b69d8937a9b796db106180ab9f31466c38b0
Amadornes <amadornes@gmail.com>
2015-02-05 17:16:27 +0100

    Added confluent redstone connections

---
+++ src/main/java/com/bluepowermod/api/wire/IConfluentConnection.java
@@ -0,0 +1,12 @@
+package com.bluepowermod.api.wire;
+
+import java.util.Collection;
+
+public interface IConfluentConnection<T> extends IConnection<T> {
+
+    /**
+     * Gets all the devices connected to A
+     */
+    public Collection<IConnection<? extends T>> getConfluentConnections();
+
+}
