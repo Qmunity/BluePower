c70689c4199a1216140cf5aa1aa18498c6138182
Quetzi <q@quetzi.net>
2015-02-17 13:24:17 +0000

    World conversion should only run on the server side

--- src/main/java/com/bluepowermod/convert/WorldConversionEventHandler.java
+++ src/main/java/com/bluepowermod/convert/WorldConversionEventHandler.java
@@ -1,11 +1,14 @@
 package com.bluepowermod.convert;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.event.world.WorldEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class WorldConversionEventHandler {
 
+    @SideOnly(Side.SERVER)
     @SubscribeEvent
     public void onWorldPreLoad(WorldEvent.Load event) {
 
