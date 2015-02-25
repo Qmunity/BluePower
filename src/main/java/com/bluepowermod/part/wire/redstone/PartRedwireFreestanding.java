018e0ce28108147bdfc96942ec752babc01e8341
Amadornes <amadornes@gmail.com>
2015-02-25 15:38:35 +0100

    Fixed freestanding wire occlusion checking *facepalm*

--- src/main/java/com/bluepowermod/part/wire/redstone/PartRedwireFreestanding.java
+++ src/main/java/com/bluepowermod/part/wire/redstone/PartRedwireFreestanding.java
@@ -77,7 +77,7 @@ import com.bluepowermod.redstone.RedstoneConnection;
 import com.bluepowermod.redstone.RedstoneConnectionCache;
 
 public abstract class PartRedwireFreestanding extends PartWireFreestanding implements IRedwire, IRedConductor, IIntegratedCircuitPart,
-IPartRedstone {
+        IPartRedstone {
 
     private RedwireType type;
 
@@ -224,7 +224,7 @@ IPartRedstone {
     }
 
     public static class PartRedwireFreestandingUninsulated extends PartRedwireFreestanding implements IAdvancedRedstoneConductor,
-    IConnectionListener {
+            IConnectionListener {
 
         private RedstoneConnectionCache connections = RedstoneApi.getInstance().createRedstoneConnectionCache(this);
         private boolean hasUpdated = false;
@@ -266,10 +266,8 @@ IPartRedstone {
             if (type == ConnectionType.STRAIGHT)
                 if (side == ForgeDirection.UNKNOWN && device instanceof DummyRedstoneDevice)
                     return false;
-            if (type == ConnectionType.CLOSED_CORNER) {
-                if (side == ForgeDirection.UNKNOWN)
-                    return false;
-            }
+            if (type == ConnectionType.CLOSED_CORNER && side == ForgeDirection.UNKNOWN)
+                return false;
 
             if (device instanceof IRedwire) {
                 RedwireType rwt = getRedwireType(side);
@@ -489,7 +487,7 @@ IPartRedstone {
     }
 
     public static class PartRedwireFreestandingInsulated extends PartRedwireFreestanding implements IAdvancedRedstoneConductor,
-    IInsulatedRedstoneDevice, IAdvancedBundledConductor, IInsulatedRedwire, IConnectionListener {
+            IInsulatedRedstoneDevice, IAdvancedBundledConductor, IInsulatedRedwire, IConnectionListener {
 
         private RedstoneConnectionCache connections = RedstoneApi.getInstance().createRedstoneConnectionCache(this);
         private BundledConnectionCache bundledConnections = RedstoneApi.getInstance().createBundledConnectionCache(this);
@@ -538,10 +536,8 @@ IPartRedstone {
             if (type == ConnectionType.STRAIGHT)
                 if (side == ForgeDirection.UNKNOWN && device instanceof DummyRedstoneDevice)
                     return false;
-            if (type == ConnectionType.CLOSED_CORNER) {
-                if (side == ForgeDirection.UNKNOWN)
-                    return false;
-            }
+            if (type == ConnectionType.CLOSED_CORNER && side == ForgeDirection.UNKNOWN)
+                return false;
 
             if (device instanceof IInsulatedRedstoneDevice) {
                 MinecraftColor c = ((IInsulatedRedstoneDevice) device).getInsulationColor(type == ConnectionType.STRAIGHT ? side
@@ -554,8 +550,7 @@ IPartRedstone {
                 RedwireType rwt = getRedwireType(side);
                 if (type == null)
                     return false;
-                RedwireType rwt_ = ((IRedwire) device).getRedwireType(type == ConnectionType.STRAIGHT ? side.getOpposite() : side
-                        .getOpposite());
+                RedwireType rwt_ = ((IRedwire) device).getRedwireType(type == ConnectionType.STRAIGHT ? side.getOpposite() : side);
                 if (rwt_ == null)
                     return false;
                 if (!rwt.canConnectTo(rwt_))
@@ -571,11 +566,30 @@ IPartRedstone {
         @Override
         public boolean canConnect(ForgeDirection side, IBundledDevice device, ConnectionType type) {
 
+            if (type == ConnectionType.STRAIGHT)
+                if (side == ForgeDirection.UNKNOWN && device instanceof DummyRedstoneDevice)
+                    return false;
+            if (type == ConnectionType.CLOSED_CORNER && side == ForgeDirection.UNKNOWN)
+                return false;
+
             if (device instanceof IInsulatedRedstoneDevice)
                 return false;
 
-            if (device instanceof IRedwire)
+            if (!OcclusionHelper.microblockOcclusionTest(getParent(), MicroblockShape.FACE_HOLLOW, 1, side))
+                return false;
+
+            if (device instanceof IRedwire) {
+                RedwireType rwt = getRedwireType(side);
+                if (type == null)
+                    return false;
+                RedwireType rwt_ = ((IRedwire) device).getRedwireType(type == ConnectionType.STRAIGHT ? side.getOpposite() : side);
+                if (rwt_ == null)
+                    return false;
+                if (!rwt.canConnectTo(rwt_))
+                    return false;
+
                 return true;
+            }
 
             return false;
         }
@@ -853,7 +867,7 @@ IPartRedstone {
     }
 
     public static class PartRedwireFreestandingBundled extends PartRedwireFreestanding implements IAdvancedBundledConductor,
-    IConnectionListener {
+            IConnectionListener {
 
         private BundledConnectionCache bundledConnections = RedstoneApi.getInstance().createBundledConnectionCache(this);
         private byte[] power = new byte[16];
@@ -947,25 +961,24 @@ IPartRedstone {
         @Override
         public boolean canConnect(ForgeDirection side, IBundledDevice device, ConnectionType type) {
 
-            if ((type == ConnectionType.STRAIGHT || type == ConnectionType.CLOSED_CORNER) && side == ForgeDirection.UNKNOWN)
+            if (type == ConnectionType.STRAIGHT)
+                if (side == ForgeDirection.UNKNOWN && device instanceof DummyRedstoneDevice)
+                    return false;
+            if (type == ConnectionType.CLOSED_CORNER && side == ForgeDirection.UNKNOWN)
                 return false;
 
             if (device instanceof IRedwire) {
                 RedwireType rwt = getRedwireType(side);
                 if (type == null)
                     return false;
-                RedwireType rwt_ = ((IRedwire) device).getRedwireType(type == ConnectionType.STRAIGHT ? side.getOpposite() : side
-                        .getOpposite());
+                RedwireType rwt_ = ((IRedwire) device).getRedwireType(type == ConnectionType.STRAIGHT ? side.getOpposite() : side);
                 if (rwt_ == null)
                     return false;
                 if (!rwt.canConnectTo(rwt_))
                     return false;
             }
 
-            if (!color.canConnect(device.getBundledColor(side.getOpposite())))
-                return false;
-
-            if (!OcclusionHelper.microblockOcclusionTest(getParent(), MicroblockShape.EDGE, 1, side, side))
+            if (!OcclusionHelper.microblockOcclusionTest(getParent(), MicroblockShape.FACE_HOLLOW, 1, side))
                 return false;
 
             return true;
