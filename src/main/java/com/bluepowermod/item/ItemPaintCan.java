2d8a07f6f7c7add9547607b6b4ed75df78365656
Amadornes <amadornes@gmail.com>
2015-05-16 23:30:47 +0200

    Tamed evil Paint Cans that like to kill servers when Shadows of Physis is installed

--- src/main/java/com/bluepowermod/item/ItemPaintCan.java
+++ src/main/java/com/bluepowermod/item/ItemPaintCan.java
@@ -10,34 +10,35 @@ package com.bluepowermod.item;
 import net.minecraft.item.ItemStack;
 
 public class ItemPaintCan extends ItemDamageableColorableOverlay {
-    
+
     private static final int MAX_USES = 16;
-    
+
     public ItemPaintCan(String name) {
-    
+
         super(name);
     }
-    
+
     @Override
     protected int getMaxUses() {
-    
+
         return MAX_USES;
     }
-    
+
     /**
-     * ItemStack sensitive version of getContainerItem.
-     * Returns a full ItemStack instance of the result.
+     * ItemStack sensitive version of getContainerItem. Returns a full ItemStack instance of the result.
      *
-     * @param itemStack The current ItemStack
+     * @param itemStack
+     *            The current ItemStack
      * @return The resulting ItemStack
      */
     @Override
     public ItemStack getContainerItem(ItemStack itemStack) {
-    
-        if (itemStack.getItemDamage() >= 16) return null;
+
+        if (itemStack.getItemDamage() >= 16)
+            return null;
         ItemStack stack = itemStack.copy();
         int newUses = getUsesUsed(stack) + 1;
-        
+
         if (newUses == getMaxUses()) {
             stack.setItemDamage(16);
             setUsesUsed(stack, 0);
@@ -46,15 +47,17 @@ public class ItemPaintCan extends ItemDamageableColorableOverlay {
         }
         return stack;
     }
-    
+
     /**
      * ItemStack sensitive version of hasContainerItem
-     * @param stack The current item stack
+     *
+     * @param stack
+     *            The current item stack
      * @return True if this item has a 'container'
      */
     @Override
     public boolean hasContainerItem(ItemStack stack) {
-    
-        return true;
+
+        return getContainerItem(stack) != null;
     }
 }
