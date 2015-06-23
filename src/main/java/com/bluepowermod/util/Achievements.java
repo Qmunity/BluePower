da5b7e2534b5d75eb5e88dccd035525f05c0a444
Amadornes <amadornes@gmail.com>
2015-06-23 20:32:42 +0200

    Finally fixed achievement localizations \o/

--- src/main/java/com/bluepowermod/util/Achievements.java
+++ src/main/java/com/bluepowermod/util/Achievements.java
@@ -12,22 +12,23 @@ import com.bluepowermod.part.PartManager;
  */
 public class Achievements {
 
-    public static Achievement vorpalAchievement         = new Achievement("achievement.bluepower:vorpal", "vorpal", -2, 0, BPItems.athame, (Achievement) null);
-    public static Achievement sortAchievement           = new Achievement("achievement.bluepower:sort", "sort", 2, 0, BPBlocks.sorting_machine,
+    public static Achievement vorpalAchievement = new Achievement("achievement.bluepower:vorpal", "bluepower:vorpal", -2, 0, BPItems.athame,
             (Achievement) null);
-    public static Achievement tungstenAchievement       = new Achievement("achievement.bluepower:tungsten", "tungsten", -2, 2, BPBlocks.tungsten_ore,
+    public static Achievement sortAchievement = new Achievement("achievement.bluepower:sort", "bluepower:sort", 2, 0, BPBlocks.sorting_machine,
             (Achievement) null);
-    public static Achievement dopeAchievement           = new Achievement("achievement.bluepower:dope", "dope", 2, 2, BPItems.blue_doped_wafer,
+    public static Achievement tungstenAchievement = new Achievement("achievement.bluepower:tungsten", "bluepower:tungsten", -2, 2,
+            BPBlocks.tungsten_ore, (Achievement) null);
+    public static Achievement dopeAchievement = new Achievement("achievement.bluepower:dope", "bluepower:dope", 2, 2, BPItems.blue_doped_wafer,
             (Achievement) null);
-    public static Achievement tubeAchievement           = new Achievement("achievement.bluepower:tube", "tube", 0, 0,
-            PartManager.getPartInfo("pneumaticTube").getItem(), (Achievement) null);
-    public static Achievement magTubeAchievement        = new Achievement("achievement.bluepower:magtube", "magtube", -2, -2,
-            PartManager.getPartInfo("magTube").getItem(), (Achievement) null);
-    public static Achievement circuitCeptionAchievement = new Achievement("achievement.bluepower:circuitception", "circuitception", 0, -2,
+    public static Achievement tubeAchievement = new Achievement("achievement.bluepower:tube", "bluepower:tube", 0, 0, PartManager.getPartInfo(
+            "pneumaticTube").getItem(), (Achievement) null);
+    public static Achievement magTubeAchievement = new Achievement("achievement.bluepower:magtube", "bluepower:magtube", -2, -2, PartManager
+            .getPartInfo("magTube").getItem(), (Achievement) null);
+    public static Achievement circuitCeptionAchievement = new Achievement("achievement.bluepower:circuitception", "bluepower:circuitception", 0, -2,
             PartManager.getPartInfo("integratedCircuit3x3").getItem(), (Achievement) null);
 
-    private static AchievementPage achievementPage = new AchievementPage("BluePower", vorpalAchievement, tungstenAchievement,
-            dopeAchievement, sortAchievement, tubeAchievement, magTubeAchievement, circuitCeptionAchievement);
+    private static AchievementPage achievementPage = new AchievementPage("BluePower", vorpalAchievement, tungstenAchievement, dopeAchievement,
+            sortAchievement, tubeAchievement, magTubeAchievement, circuitCeptionAchievement);
 
     public static void init() {
 
