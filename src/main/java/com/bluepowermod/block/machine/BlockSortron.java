5f2a3618b8f7be912bd1d51a8fcd6e43dad6d71f
MoreThanHidden <john816_@hotmail.com>
2019-03-31 21:28:45 +0800

    Fixes #474 Removed historical sprite references.

--- src/main/java/com/bluepowermod/block/machine/BlockSortron.java
+++ src/main/java/com/bluepowermod/block/machine/BlockSortron.java
@@ -12,11 +12,6 @@ import com.bluepowermod.reference.GuiIDs;
 import com.bluepowermod.reference.Refs;
 import com.bluepowermod.tile.tier3.TileSortron;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 ;
 
@@ -24,8 +19,6 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  * @author Dynious
  */
 public class BlockSortron extends BlockContainerBase {
-    
-    private final TextureAtlasSprite[] icons = new TextureAtlasSprite[8];
     
     public BlockSortron() {
     
@@ -40,37 +33,5 @@ public class BlockSortron extends BlockContainerBase {
         return GuiIDs.INVALID;
     }
 
-    
-    /* @Override
-     @SideOnly(Side.CLIENT)
-     public TextureAtlasSprite getIcon(IBlockAccess world, int x, int y, int z, int side) {
-     
-         
-                 TileSortron tile = (TileSortron) world.getTileEntity(x, y, z);
-                 EnumFacing dir = tile.getFacingDirection();
-                 if (dir.ordinal() == side) {
-                     return icons[0];
-                 } else if (dir.getOpposite().ordinal() == side) {
-                     return icons[1];
-                 } else if ((side == 1 && dir.ordinal() != 1 && dir.getOpposite().ordinal() != 1) || (side == 3 && (dir.ordinal() == 1 || dir.getOpposite().ordinal() == 1))) { // && isPowered
-                     return icons[6];
-                 } else if (tile.showOutPutAnimation()) { // && isPowered
-                     return icons[4];
-                 } else { // && isPowered
-                     return icons[3];
-                 }
-         //TODO: different icons when powered
-     }*/
 
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(TextureMap iconRegister) {
-    
-        icons[0] = iconRegister.registerSprite(new ResourceLocation(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKSORTRON_NAME + "_front"));
-        icons[1] = iconRegister.registerSprite(new ResourceLocation(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKSORTRON_NAME + "_back"));
-        icons[2] = iconRegister.registerSprite(new ResourceLocation(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKSORTRON_NAME + "_side_off"));
-        icons[3] = iconRegister.registerSprite(new ResourceLocation(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKSORTRON_NAME + "_side_on"));
-        icons[4] = iconRegister.registerSprite(new ResourceLocation(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKSORTRON_NAME + "_side_on_1"));
-        icons[5] = iconRegister.registerSprite(new ResourceLocation(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKSORTRON_NAME + "_side"));
-        icons[6] = iconRegister.registerSprite(new ResourceLocation(Refs.MODID + ":" + Refs.MACHINE_TEXTURE_LOCATION + Refs.BLOCKSORTRON_NAME + "_side_active"));
-    }
 }
