61fd1360055b8a20ef9602a063a1b49161c9c908
MoreThanHidden <john816_@hotmail.com>
2019-07-30 00:05:20 +0800

    Smelting and Alloy Furnace Recipes

--- src/main/java/com/bluepowermod/init/Recipes.java
+++ src/main/java/com/bluepowermod/init/Recipes.java
@@ -17,7 +17,6 @@
 
 package com.bluepowermod.init;
 
-import com.bluepowermod.recipe.CoreRecipes;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
@@ -29,28 +28,8 @@ public class Recipes {
 
     public static void init() {
 
-        CoreRecipes.init();
-        //LogicRecipes.init();
-        //MachineRecipes.init();
-        //LightingRecipes.init();
-
         // Alloy furnace
         IAlloyFurnaceRegistry af = BPApi.getInstance().getAlloyFurnaceRegistry();
-
-        af.addRecipe(new ItemStack(BPItems.red_alloy_ingot, 1), new ItemStack(Items.REDSTONE, 4), Items.IRON_INGOT);
-        af.addRecipe(new ItemStack(BPItems.red_alloy_ingot, 1), new ItemStack(Items.REDSTONE, 4), BPItems.copper_ingot);
-
-        af.addRecipe(new ItemStack(BPItems.blue_alloy_ingot, 1), new ItemStack(BPItems.teslatite_dust, 4), BPItems.silver_ingot);
-
-        af.addRecipe(new ItemStack(BPItems.purple_alloy_ingot, 1), BPItems.blue_alloy_ingot, BPItems.red_alloy_ingot);
-        af.addRecipe(new ItemStack(BPItems.purple_alloy_ingot, 1), Items.GOLD_INGOT, new ItemStack(BPItems.infused_teslatite_dust, 8));
-
-        af.addRecipe(new ItemStack(BPItems.brass_ingot, 4), new ItemStack(BPItems.copper_ingot, 3), BPItems.zinc_ingot);
-        af.addRecipe(new ItemStack(BPItems.silicon_boule, 1), new ItemStack(Items.COAL, 8), new ItemStack(Blocks.SAND, 8));
-
-        af.addRecipe(new ItemStack(BPItems.red_doped_wafer, 1), new ItemStack(Items.REDSTONE, 4), BPItems.silicon_wafer);
-        af.addRecipe(new ItemStack(BPItems.blue_doped_wafer, 1), new ItemStack(BPItems.teslatite_dust, 4), BPItems.silicon_wafer);
-        af.addRecipe(new ItemStack(BPItems.zincplate, 4), new ItemStack(BPItems.zinc_ingot, 1), new ItemStack(Items.IRON_INGOT, 2));
 
         af.addRecyclingRecipe(new ItemStack(Blocks.IRON_BLOCK), new ItemStack(Items.IRON_INGOT, 9));
         af.addRecyclingRecipe(new ItemStack(Blocks.GOLD_BLOCK), new ItemStack(Items.GOLD_INGOT, 9));
@@ -58,8 +37,5 @@ public class Recipes {
         af.addRecyclingRecipe(new ItemStack(Items.GOLD_INGOT));
         af.addRecyclingRecipe(new ItemStack(Items.GOLD_NUGGET));
 
-        af.addRecipe(new ItemStack(BPBlocks.sapphire_glass, 4), new ItemStack(Blocks.GLASS, 4), BPItems.sapphire_gem);
-        af.addRecipe(new ItemStack(BPBlocks.reinforced_sapphire_glass, 1), new ItemStack(BPBlocks.sapphire_glass, 1), new ItemStack(
-                Blocks.OBSIDIAN, 5));
     }
 }
