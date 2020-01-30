b279626a6ab50927a59ff88f7774185deec5d0c0
MoreThanHidden <john816_@hotmail.com>
2020-01-31 00:52:05 +0800

    Cleanup the Alloy Furnace Data Generator to avoid crashing the registry Possible Fix / Mitigation for #536

--- src/main/java/com/bluepowermod/util/DatapackUtils.java
+++ src/main/java/com/bluepowermod/util/DatapackUtils.java
@@ -6,11 +6,14 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * Data Pack Generator functions closely derived from "MoreThanHidden/RestrictedPortals" - AdvancementHelper
@@ -22,51 +25,47 @@ public class DatapackUtils {
      * Generates an Alloy Furnace Recipe Data Pack.
      */
     public static void generateAlloyFurnaceRecipe(IAlloyFurnaceRecipe recipe, String path) {
-
-        //Name the file
-        String shortname = recipe.getRequiredItems().get(0).getMatchingStacks()[0].getItem().getRegistryName().getPath() +
-                recipe.getRecipeOutput().getItem().getRegistryName().getPath() + "_generated";
-
-        //Serialize Json
-        JsonArray ingredientArray = new JsonArray();
-
-        for (int i = 0; i < recipe.getRequiredItems().size(); i++) {
-            JsonObject ingredients = new JsonObject();
-            ingredients.add("item", new JsonPrimitive(recipe.getRequiredItems().get(i).getMatchingStacks()[0].getItem().getRegistryName().toString()));
-            ingredients.add("count", new JsonPrimitive(recipe.getRequiredCount().get(i)));
-            ingredientArray.add(ingredients);
-        }
-
-        //Output Item
-        ItemStack outputItem = recipe.getRecipeOutput();
-        JsonObject outitem = new JsonObject();
-        outitem.add("item", new JsonPrimitive(outputItem.getItem().getRegistryName().toString()));
-        outitem.add("count", new JsonPrimitive(outputItem.getCount()));
-
-        JsonObject json = new JsonObject();
-        json.add("type", new JsonPrimitive("bluepower:alloy_smelting"));
-        json.add("ingredients", ingredientArray);
-        json.add("result", outitem);
-
-        //Create Json File
-        File file = new File(path + "/bluepower/data/bluepower/recipes/alloy_furnace/" + shortname + ".json");
         try {
-            if(file.createNewFile()){
-                //BluePower.log.info("Created Json File: " + file.getPath());
-            }else{
-                //BluePower.log.info("File already Exists: " + file.getPath());
+            Optional<Ingredient> ingredient = recipe.getRequiredItems().stream().findFirst();
+            if(ingredient.isPresent()) {
+                Optional<ItemStack> requiredItem = Arrays.stream(ingredient.get().getMatchingStacks()).findFirst();
+                if (requiredItem.isPresent()) {
+                    //Name the file
+                    String shortname = requiredItem.get().getItem().getRegistryName().getPath() +
+                            recipe.getRecipeOutput().getItem().getRegistryName().getPath() + "_generated";
+
+                    //Serialize Json
+                    JsonArray ingredientArray = new JsonArray();
+
+                    for (int i = 0; i < recipe.getRequiredItems().size(); i++) {
+                        JsonObject ingredients = new JsonObject();
+                        ingredients.add("item", new JsonPrimitive(recipe.getRequiredItems().get(i).getMatchingStacks()[0].getItem().getRegistryName().toString()));
+                        ingredients.add("count", new JsonPrimitive(recipe.getRequiredCount().get(i)));
+                        ingredientArray.add(ingredients);
+                    }
+
+                    //Output Item
+                    ItemStack outputItem = recipe.getRecipeOutput();
+                    JsonObject outitem = new JsonObject();
+                    outitem.add("item", new JsonPrimitive(outputItem.getItem().getRegistryName().toString()));
+                    outitem.add("count", new JsonPrimitive(outputItem.getCount()));
+
+                    JsonObject json = new JsonObject();
+                    json.add("type", new JsonPrimitive("bluepower:alloy_smelting"));
+                    json.add("ingredients", ingredientArray);
+                    json.add("result", outitem);
+
+                    //Create Json File
+                    File file = new File(path + "/bluepower/data/bluepower/recipes/alloy_furnace/" + shortname + ".json");
+
+                    FileWriter fileWriter = new FileWriter(file.getPath());
+                    fileWriter.write(json.toString());
+                    fileWriter.close();
+                }
             }
-        } catch (IOException e) {
-            //BluePower.log.info("Creating file failed: " + file.getPath());
-        }
-
-        try {
-            FileWriter fileWriter = new FileWriter(file.getPath());
-            fileWriter.write(json.toString());
-            fileWriter.close();
 
         } catch (IOException e) {
-            //BluePower.log.info("Editing File Failed: " + file.getPath());
+                BluePower.log.error("Error creating Alloy Furnace recipe - " + e.getLocalizedMessage());
         }
 
     }
