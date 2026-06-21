48d6d1efc4ff0e6b49d85b1747936b95c951b5d9
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-06-21 12:21:47 -0400

    made buffer gate only able to have left and right sides disabled

--- src/main/java/com/bluepowermod/block/gates/BlockGateBuffer.java
+++ src/main/java/com/bluepowermod/block/gates/BlockGateBuffer.java
@@ -1,8 +1,29 @@
 package com.bluepowermod.block.gates;
 
+import com.bluepowermod.tile.tier1.TileGate;
+import net.minecraft.world.level.block.state.BlockState;
+
 public class BlockGateBuffer extends BlockGateNot{
     @Override
     protected byte getOutput(byte back) {
         return (byte) (back > 0 ? 16 : 0);
+    }
+
+
+    @Override
+    protected boolean cycleDisabledStates(BlockState state, TileGate gate) {
+        boolean left = gate.isDisabled(Side.LEFT), right = gate.isDisabled(Side.RIGHT);
+        if (!left && !right) {
+            gate.setDisabled(Side.RIGHT, true);
+        } else if (!left) {
+            gate.setDisabled(Side.LEFT, true);
+            gate.setDisabled(Side.RIGHT, false);
+        } else if (!right){
+            gate.setDisabled(Side.RIGHT, true);
+        }else {// right enabled
+            gate.setDisabled(Side.LEFT, false);
+            gate.setDisabled(Side.RIGHT, false);
+        }
+        return true;
     }
 }
