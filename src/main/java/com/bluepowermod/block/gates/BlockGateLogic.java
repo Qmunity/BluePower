881ebeb3ff2e9c2dde63ebf6d3a0a91d05ea42e6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-06-23 16:27:18 -0400

    made BlockGateLogic pass onTick boolean to update states method

--- src/main/java/com/bluepowermod/block/gates/BlockGateLogic.java
+++ src/main/java/com/bluepowermod/block/gates/BlockGateLogic.java
@@ -12,6 +12,6 @@ public abstract class BlockGateLogic extends BlockGateBase {
     @Override
     protected boolean checkPower(BlockState state, TileGate gate, boolean onTick){
         Map<Side, Byte> map = getSidePower(state, gate);
-        return gate.updateStates(map, true);
+        return gate.updateStates(map, !onTick);
     }
 }
